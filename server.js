0e90fa02bf299169ff7ff11bfe204fbcce9dad60
sabsrolz <sabsrolz@umich.edu>
2019-10-21 14:48:03 -0500

    created server using express

---
+++ server.js
@@ -0,0 +1,16 @@
+//import node packages
+const express = require("express");
+const path = require("path");
+
+//set up express app
+let app = express();
+const PORT = process.env.PORT || 8080;
+
+//set up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//start the server to begin listening
+app.listen(PORT, function() {
+  console.log(`app is listening on port ${PORT} `);
+});
