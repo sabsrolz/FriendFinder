93258f50c8a41799defd4b53889168e11280fccc
sabsrolz <sabsrolz@umich.edu>
2019-10-25 16:38:01 -0500

    outputted images in html for friend match

--- app/routing/friends_jaws.sql
+++ app/routing/friends_jaws.sql
@@ -24,11 +24,17 @@ ADD COLUMN score8 INTEGER(10) NOT NULL,
 ADD COLUMN score9 INTEGER(10) NOT NULL,
 ADD COLUMN score10 INTEGER(10) NOT NULL; 
 
+ALTER TABLE friends
+MODIFY image_url VARCHAR(500) NOT NULL;
+
 SELECT * FROM friends;
 
-INSERT INTO friends (friend_name, image_url, score1, score2, score3, score4, score5, score6, score7, score8, score9, score10) VALUES ("Aysu", "img", 2, 1, 2, 1, 3, 1, 2, 3, 1, 1);
-INSERT INTO friends (friend_name, image_url, score1, score2, score3, score4, score5, score6, score7, score8, score9, score10) VALUES ("Helen", "img", 1, 1, 1, 3, 3, 2, 1, 3, 1, 1);
-INSERT INTO friends (friend_name, image_url, score1, score2, score3, score4, score5, score6, score7, score8, score9, score10) VALUES ("Ahmed", "img", 3, 3, 3, 2, 2, 3, 1, 2, 1, 1);
+DELETE FROM friends;
+
+INSERT INTO friends (friend_name, image_url, score1, score2, score3, score4, score5, score6, score7, score8, score9, score10) VALUES ("Aysu", "https://www.clipartwiki.com/clipimg/detail/12-122806_transparent-stick-figure-clipart-stick-figure-transparent-background.png", 2, 1, 2, 1, 3, 1, 2, 3, 1, 1);
+INSERT INTO friends (friend_name, image_url, score1, score2, score3, score4, score5, score6, score7, score8, score9, score10) VALUES ("Helen", "https://www.pngtube.com/myfile/detail/1-17145_girl-clipart-person-woman-stick-figure-clipart.png", 1, 1, 1, 3, 3, 2, 1, 3, 1, 1);
+INSERT INTO friends (friend_name, image_url, score1, score2, score3, score4, score5, score6, score7, score8, score9, score10) VALUES ("Ahmed", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTEqMr_79jr3iHnDa0u2yUCZzmTzT0ujcVbvzRPFEZicmOvCAJM", 3, 3, 3, 2, 2, 3, 1, 2, 1, 1);
+
 
 
 
