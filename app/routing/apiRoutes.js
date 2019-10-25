308c255de08c84c556b9dacf7d1d511d41a3314f
sabsrolz <sabsrolz@umich.edu>
2019-10-25 08:46:12 -0500

    added code for Heroku database

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,27 @@
 //let friends = require("../data/friends");
 
 const mysql = require("mysql");
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Coquisolz18",
-  database: "friends_db"
-});
+// let connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Coquisolz18",
+//   database: "friends_db"
+// });
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "Coquisolz18",
+    database: "friends_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
